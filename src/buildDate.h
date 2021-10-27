7ca7e39ef6409aac5e3b994c2d09d59e47906f69
a.borisov <a.borisov@gaijin.team>
2021-10-27 19:05:52 +0300

    changed version to 0.1.22

--- src/buildDate.h
+++ src/buildDate.h
@@ -1,2 +1,2 @@
 #pragma once
-#define DASBOX_BUILD_DATE "26.10.2021"
+#define DASBOX_BUILD_DATE "27.10.2021"
--- src/globals.h
+++ src/globals.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DASBOX_VERSION "0.1.21"
+#define DASBOX_VERSION "0.1.22"
 
 namespace sf
 {

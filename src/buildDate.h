a1255b9e021d0875b7bd4fade8565e638e79ba7a
a.borisov <a.borisov@gaijin.team>
2021-11-05 09:37:31 +0300

    sound: initial support of FFT

--- src/buildDate.h
+++ src/buildDate.h
@@ -1,2 +1,2 @@
 #pragma once
-#define DASBOX_BUILD_DATE "3.11.2021"
+#define DASBOX_BUILD_DATE "4.11.2021"
--- src/sound.cpp
+++ src/sound.cpp
@@ -3,8 +3,10 @@
 #include <daScript/daScript.h>
 #include <daScript/simulate/interop.h>
 #include <daScript/simulate/simulate_visit_op.h>
+#include <minfft.h>
 #include <array>
 #include <unordered_set>
+#include <unordered_map>
 #include "globals.h"
 #include "fileSystem.h"
 
@@ -37,6 +39,8 @@ static float master_volume = 1.0f;
 
 static unordered_set<float *> sound_data_pointers;
 static unordered_set<DasboxDebugInfo *> dbg_pointers;
+static unordered_map<int, minfft_aux *> fft_complex_aux_pointers;
+static unordered_map<int, minfft_aux *> fft_real_aux_pointers;
 
 int get_total_sound_count()
 {
@@ -880,8 +884,113 @@ void delete_allocated_sounds()
     delete dbg;
 
   dbg_pointers.clear();
+
+  for (auto && aux : fft_complex_aux_pointers)
+    minfft_free_aux(aux.second);
+
+  fft_complex_aux_pointers.clear();
+
+  for (auto && aux : fft_real_aux_pointers)
+    minfft_free_aux(aux.second);
+
+  fft_real_aux_pointers.clear();
 }
 
+static_assert(sizeof(minfft_real) == sizeof(float), "sizeof(minfft_real) != sizeof(float), define MINFFT_SINGLE");
+
+
+typedef minfft_aux * (*fft_aux_fn)(int);
+
+inline minfft_aux * allocate_fft_aux(unordered_map<int, minfft_aux *> & container, int n, fft_aux_fn fn)
+{
+  auto it = container.find(n);
+  if (it == container.end())
+  {
+    container[n] = fn(n);
+    it = container.find(n);
+  }
+  return it->second;
+}
+
+void fft_real_forward(const das::TArray<float> & real_signal, das::TArray<float2> & complex_frequencies, das::Context * context)
+{
+  if (real_signal.size == 0 || ((real_signal.size + 1) & real_signal.size) == 0)
+  {
+    print_error("fft_real_forward: Length of input array must be power of 2 (length = %d)", int(real_signal.size));
+    return;
+  }
+
+  minfft_aux * aux = allocate_fft_aux(fft_real_aux_pointers, real_signal.size, minfft_mkaux_realdft_1d);
+  int complexOutSize = real_signal.size / 2 + 1;
+  if (complex_frequencies.size != complexOutSize)
+    builtin_array_resize(complex_frequencies, complexOutSize, complex_frequencies.stride, context);
+
+  minfft_realdft((minfft_real *)real_signal.data, (minfft_cmpl *)complex_frequencies.data, aux);
+}
+
+
+void fft_calculate_magnitudes(const das::TArray<float2> & complex_frequencies, das::TArray<float> & magnitudes, das::Context * context)
+{
+  if (magnitudes.size != complex_frequencies.size)
+    builtin_array_resize(magnitudes, complex_frequencies.size, magnitudes.stride, context);
+
+  float2 * __restrict c = (float2 *)complex_frequencies.data;
+  float * __restrict m = (float *)magnitudes.data;
+  for (int i = 0, len = complex_frequencies.size; i < len; i++, m++, c++)
+    *m = sqrtf(sqr(c->x) + sqr(c->y));
+}
+
+void fft_calculate_normalized_magnitudes(const das::TArray<float2> & complex_frequencies, das::TArray<float> & magnitudes, das::Context * context)
+{
+  if (magnitudes.size != complex_frequencies.size)
+    builtin_array_resize(magnitudes, complex_frequencies.size, magnitudes.stride, context);
+
+  if (!complex_frequencies.size)
+    return;
+
+  int len = complex_frequencies.size;
+  float inv = 1.0f / len;
+
+  float2 * __restrict c = (float2 *)complex_frequencies.data;
+  float * __restrict m = (float *)magnitudes.data;
+  for (int i = 0; i < len; i++, m++, c++)
+    *m = sqrtf(sqr(c->x) + sqr(c->y)) * inv;
+}
+
+void fft_calculate_log_magnitudes(const das::TArray<float2> & complex_frequencies, das::TArray<float> & magnitudes, das::Context * context)
+{
+  if (magnitudes.size != complex_frequencies.size)
+    builtin_array_resize(magnitudes, complex_frequencies.size, magnitudes.stride, context);
+
+  if (!complex_frequencies.size)
+    return;
+
+  int len = complex_frequencies.size;
+  float offset = log2f(len);
+
+  float2 * __restrict c = (float2 *)complex_frequencies.data;
+  float * __restrict m = (float *)magnitudes.data;
+  for (int i = 0; i < len; i++, m++, c++)
+    *m = log2(sqr(c->x) + sqr(c->y) + 1e-10f) - offset;
+}
+
+
+void fft_real_inverse(const das::TArray<float2> & complex_frequencies, das::TArray<float> & real_signal, das::Context * context)
+{
+  int p2 = complex_frequencies.size - 1;
+  if (complex_frequencies.size <= 0 || ((p2 + 1) & p2) == 0)
+  {
+    print_error("fft_real_inverse: Length of input array must be (power of 2)+1, but length = %d)", int(complex_frequencies.size));
+    return;
+  }
+
+  minfft_aux * aux = allocate_fft_aux(fft_real_aux_pointers, p2 * 2, minfft_mkaux_realdft_1d);
+  int realOutSize = p2 * 2;
+  if (complex_frequencies.size != realOutSize)
+    builtin_array_resize(real_signal, realOutSize, real_signal.stride, context);
+
+  minfft_invrealdft((minfft_cmpl *)complex_frequencies.data, (minfft_real *)real_signal.data, aux);
+}
 
 
 PlayingSoundHandle play_sound_internal(const PcmSound & sound, float volume, float pitch, float pan, float start_time, float end_time,
@@ -1475,6 +1584,27 @@ public:
 
     addExtern<DAS_BIND_FUN(sound::get_total_time_played)>(*this, lib,
       "get_total_time_played", SideEffects::accessExternal, "get_total_time_played");
+
+
+    addExtern<DAS_BIND_FUN(sound::fft_real_forward)>(*this, lib,
+      "fft_real_forward", SideEffects::modifyArgumentAndExternal, "fft_real_forward")
+      ->args({"real_signal", "complex_frequencies", ""});
+
+    addExtern<DAS_BIND_FUN(sound::fft_real_inverse)>(*this, lib,
+      "fft_real_inverse", SideEffects::modifyArgumentAndExternal, "fft_real_inverse")
+      ->args({"complex_frequencies", "real_signal", ""});
+
+    addExtern<DAS_BIND_FUN(sound::fft_calculate_magnitudes)>(*this, lib,
+      "fft_calculate_magnitudes", SideEffects::modifyArgumentAndExternal, "fft_calculate_magnitudes")
+      ->args({"complex_frequencies", "magnitudes", ""});
+
+    addExtern<DAS_BIND_FUN(sound::fft_calculate_normalized_magnitudes)>(*this, lib,
+      "fft_calculate_normalized_magnitudes", SideEffects::modifyArgumentAndExternal, "fft_calculate_normalized_magnitudes")
+      ->args({"complex_frequencies", "normalized_magnitudes", ""});
+
+    addExtern<DAS_BIND_FUN(sound::fft_calculate_log_magnitudes)>(*this, lib,
+      "fft_calculate_log_magnitudes", SideEffects::modifyArgumentAndExternal, "fft_calculate_log_magnitudes")
+      ->args({"complex_frequencies", "log_magnitudes", ""});
 
     // its AOT ready
     //verifyAotReady();

8866e70ee3c814c2593b9fd0ba5f64f97562796a
a.borisov <a.borisov@gaijin.team>
2022-03-04 23:10:01 +0300

    change version to 0.1.72

--- src/buildDate.h
+++ src/buildDate.h
@@ -1,2 +1,2 @@
 #pragma once
-#define DASBOX_BUILD_DATE "3.3.2022"
+#define DASBOX_BUILD_DATE "4.3.2022"
--- src/globals.h
+++ src/globals.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DASBOX_VERSION "0.1.71"
+#define DASBOX_VERSION "0.1.72"
 
 #include <string>
 
